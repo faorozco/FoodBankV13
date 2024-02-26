d3a4d9fee2246073c97ed4ee39a0c1d5803f4983
Juan Piedrahita <jmpiedra@bancolombia.com.co>
2024-02-26 14:59:17 -0500

    Correccion de buton home

--- src/app/constants/home-buttons..const.ts
+++ src/app/constants/home-buttons..const.ts
@@ -1,27 +1,27 @@
 export const HOME_BUTTONS = [
     {
-        text: 'Donate with love to our beneficiaries of the Foundation Maria Luisa de Moreno International',
+        text: 'Donate with love to our beneficiaries of the  Maria Luisa de Moreno International Foundation',
         router: './list-beneficiaries',
         button: 'I love to help',
         pathImag: '../../../assets/imgs/donate.jpg'
 
     },
     {
-        text: 'Create a new beneficiary of the Foundation Maria Luisa de Moreno International',
+        text: 'Create a new beneficiary of the  Maria Luisa de Moreno International Foundation',
         router: './new-beneficiary',
         button: 'New beneficiary',
         pathImag: '../../../assets/imgs/newBeneficiary.jpg'
 
     },
     {
-        text: 'List, update or delete beneficiaries of the Foundation Maria Luisa de Moreno International',
+        text: 'List, update or delete beneficiaries of the  Maria Luisa de Moreno International Foundation',
         router: './list-beneficiaries',
         button: 'List of beneficiaries',
         pathImag: '../../../assets/imgs/list-beneficiary.jpg'
 
     },
     {
-        text: 'Listar y administrar de las entregas of the Foundation Maria Luisa de Moreno International',
+        text: 'list and manage deliveries of the  Maria Luisa de Moreno International Foundation',
         router: './deliveries',
         button: 'List of deliveries',
         pathImag: '../../../assets/imgs/list-deliveries.jpg'
