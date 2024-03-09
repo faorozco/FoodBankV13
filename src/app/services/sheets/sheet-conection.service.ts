09aa99191e1cdbc293a7cc569765286c85b78b46
monroypiedrahita <monroypiedrahita2@gmail.com>
2024-03-09 12:47:07 -0500

    se hace doble BD una para dev y otra para oficial

--- src/app/services/sheets/sheet-conection.service.ts
+++ src/app/services/sheets/sheet-conection.service.ts
@@ -5,36 +5,40 @@ import { BeneficiaryModel } from '../../models/beneficiary.model';
   providedIn: 'root',
 })
 export class SheetConectionService {
-  listBeneficiariesDEV = 'https://sheet.best/api/sheets/6e67b075-8024-4ffa-8668-6d9b7a3459a3';
-  EntregadosDevApi = 'https://sheet.best/api/sheets/a0cb4b83-bcaa-4094-81f1-45a53cebf949';
-  listBeneficiariesAPIFIMLM = 'https://sheet.best/api/sheets/add0cfce-5d03-4ad9-9053-974d6f8a8f80'
+  listBeneficiariesDEV =
+    'https://sheet.best/api/sheets/6e67b075-8024-4ffa-8668-6d9b7a3459a3';
+  EntregadosDevApi =
+    'https://sheet.best/api/sheets/a0cb4b83-bcaa-4094-81f1-45a53cebf949';
+  listBeneficiariesAPIFIMLM =
+    'https://sheet.best/api/sheets/add0cfce-5d03-4ad9-9053-974d6f8a8f80';
+  listDeliveriesAPIFIMLM =
+    'https://sheet.best/api/sheets/81b81f8c-e36a-4c28-8d4c-4da48483aa4f';
 
   constructor(public http: HttpClient) {}
 
   newBeneficiary(body: BeneficiaryModel) {
-    let url = this.listBeneficiariesAPIFIMLM
-    const rol = JSON.parse(localStorage.getItem('rol')!)
-    if(rol === 'DEV') {
-      url = this.listBeneficiariesDEV
+    let url = this.listBeneficiariesAPIFIMLM;
+    const rol = JSON.parse(localStorage.getItem('rol')!);
+    if (rol === 'DEV') {
+      url = this.listBeneficiariesDEV;
     }
     return this.http.post<BeneficiaryModel>(url, body);
   }
 
-
   getAllBeneficiaries() {
-    let url = this.listBeneficiariesAPIFIMLM
-    const rol = JSON.parse(localStorage.getItem('rol')!)
-    if(rol === 'DEV') {
-      url = this.listBeneficiariesDEV
+    let url = this.listBeneficiariesAPIFIMLM;
+    const rol = JSON.parse(localStorage.getItem('rol')!);
+    if (rol === 'DEV') {
+      url = this.listBeneficiariesDEV;
     }
     return this.http.get<any>(url);
   }
 
   deleteBeneficiary(id: string) {
-    let url = this.listBeneficiariesAPIFIMLM
-    const rol = JSON.parse(localStorage.getItem('rol')!)
-    if(rol === 'DEV') {
-      url = this.listBeneficiariesDEV
+    let url = this.listBeneficiariesAPIFIMLM;
+    const rol = JSON.parse(localStorage.getItem('rol')!);
+    if (rol === 'DEV') {
+      url = this.listBeneficiariesDEV;
     }
     return this.http.delete(`${url}/DocumentNumber/${id}`);
   }
@@ -45,26 +49,39 @@ export class SheetConectionService {
     return dateTimeString;
   }
 
-  updateBeneficiary(id: number, body: BeneficiaryModel){
-    let url = this.listBeneficiariesAPIFIMLM
-    const rol = JSON.parse(localStorage.getItem('rol')!)
-    if(rol === 'DEV') {
-      url = this.listBeneficiariesDEV
+  updateBeneficiary(id: number, body: BeneficiaryModel) {
+    let url = this.listBeneficiariesAPIFIMLM;
+    const rol = JSON.parse(localStorage.getItem('rol')!);
+    if (rol === 'DEV') {
+      url = this.listBeneficiariesDEV;
     }
     return this.http.put(`${url}/${id}`, body);
   }
 
   delivery(body: BeneficiaryModel) {
-    return this.http.post<BeneficiaryModel>(this.EntregadosDevApi, body);
+    let url = this.listDeliveriesAPIFIMLM;
+    const rol = JSON.parse(localStorage.getItem('rol')!);
+    if (rol === 'DEV') {
+      url = this.EntregadosDevApi;
+    }
+    return this.http.post<BeneficiaryModel>(url, body);
   }
 
-
   getAllDeliveries() {
-    return this.http.get<any>(this.EntregadosDevApi);
+    let url = this.listDeliveriesAPIFIMLM;
+    const rol = JSON.parse(localStorage.getItem('rol')!);
+    if (rol === 'DEV') {
+      url = this.EntregadosDevApi;
+    }
+    return this.http.get<any>(url);
   }
 
   deleteDelivery(id: number) {
-    return this.http.delete(`${this.EntregadosDevApi}/${id}`);
+    let url = this.listDeliveriesAPIFIMLM;
+    const rol = JSON.parse(localStorage.getItem('rol')!);
+    if (rol === 'DEV') {
+      url = this.EntregadosDevApi;
+    }
+    return this.http.delete(`${url}/${id}`);
   }
-
 }
