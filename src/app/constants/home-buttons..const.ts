114746e2b2ace54234638cd0012500e8b2c38373
monroypiedrahita <monroypiedrahita2@gmail.com>
2024-03-14 22:33:07 -0500

    Cambios para revision

--- src/app/constants/home-buttons..const.ts
+++ src/app/constants/home-buttons..const.ts
@@ -1,23 +1,23 @@
 export const HOME_BUTTONS = [
-    {
-        text: 'Donate with love to our beneficiaries of the  Maria Luisa de Moreno International Foundation',
-        router: '../list-beneficiaries',
-        button: 'List of beneficiaries',
-        pathImag: '../../../assets/imgs/donate.jpg',
-
-    },
-    {
-        text: 'Create a new beneficiary of the  Maria Luisa de Moreno International Foundation',
-        router: '../new-beneficiary',
-        button: 'New beneficiary',
-        pathImag: '../../../assets/imgs/newBeneficiary.jpg',
-
-    },
-    {
-        text: 'List and administration the list of deliveries of the  Maria Luisa de Moreno International Foundation',
-        router: '../administration',
-        button: 'List of Deliveries',
-        pathImag: '../../../assets/imgs/list-beneficiary.jpg',
-
-    },
-]
+  {
+    text: 'Donate with love to our beneficiaries of the  Maria Luisa de Moreno International Foundation',
+    router: '../list-beneficiaries',
+    button: 'List of beneficiaries',
+    // pathImag: '../../../assets/imgs/donate.jpg',
+    pathImag: '../../../assets/imgs/help.JPG',
+  },
+  {
+    text: 'Create a new beneficiary of the  Maria Luisa de Moreno International Foundation',
+    router: '../new-beneficiary',
+    button: 'New beneficiary',
+    // pathImag: '../../../assets/imgs/newBeneficiary.jpg',
+    pathImag: '../../../assets/imgs/newJPG.JPG',
+  },
+  {
+    text: 'List and administration the list of deliveries of the  Maria Luisa de Moreno International Foundation',
+    router: '../administration',
+    button: 'List of Deliveries',
+    // pathImag: '../../../assets/imgs/list-beneficiary.jpg',
+    pathImag: '../../../assets/imgs/mercados.jpg',
+  },
+];
